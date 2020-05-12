8603ffb9dc0e7cb3cc9226dccb33c65b5e7b4e96
Will <wehb@hotmail.co.uk>
2020-05-12 23:50:28 +0100

    Hello Triangle!

--- Source/Graphics/Renderer/Renderer.h
+++ Source/Graphics/Renderer/Renderer.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <vector>
 #include <memory>
 
 class GWindow;
+class GraphicsShader;
 
 class GRenderer
 {
@@ -24,10 +26,24 @@ private:
 
 	int PostWindowInit();
 
+	void CompileShaders();
+
+	bool CompileShader(const char* ShaderSource, GraphicsShader& Shader);
+
+	bool LinkShader(GraphicsShader& ShaderProgram, std::vector<GraphicsShader> ShaderArray);
+
 	void RenderScene();
 
 	void Terminate();
 
 private:
-	std::unique_ptr<GWindow> Window;
+	std::unique_ptr<GWindow> WindowPtr;
+
+	// Hello Triangle
+	std::unique_ptr<GraphicsShader> VertexShaderPtr;
+	std::unique_ptr<GraphicsShader> FragmentShaderPtr;
+	std::unique_ptr<GraphicsShader> ShaderProgramPtr;
+
+	unsigned int VAO = 0;
+	unsigned int VBO = 0;
 };
--- Source/Graphics/Renderer/Source/Renderer.cpp
+++ Source/Graphics/Renderer/Source/Renderer.cpp
@@ -1,10 +1,31 @@
 #include "Graphics/Renderer/Renderer.h"
 #include "Graphics/Window/Window.h"
+#include "Common/DebugMacros.h"
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+
+class GraphicsShader
+{
+public:
+    GraphicsShader()
+    {
+    }
+
+    GraphicsShader(const GLuint& InShader)
+        : GLShader(InShader)
+    {
+    }
+
+    operator GLuint()
+    {
+        return GLShader;
+    }
+
+    GLuint GLShader = 0;
+};
 
 GRenderer::GRenderer()
 {
@@ -12,6 +33,14 @@ GRenderer::GRenderer()
 
 GRenderer::~GRenderer()
 {
+    glDeleteShader(*VertexShaderPtr);
+    glDeleteShader(*FragmentShaderPtr);
+    glDeleteProgram(*ShaderProgramPtr);
+
+    VertexShaderPtr.release();
+    FragmentShaderPtr.release();
+    ShaderProgramPtr.release();
+
     Terminate();
 }
 
@@ -27,13 +56,13 @@ void GRenderer::Init()
 
 void GRenderer::Run()
 {
-    while (!Window->ShouldClose())
+    while (!WindowPtr->ShouldClose())
     {
-        Window->ProcessInput();
+        WindowPtr->ProcessInput();
 
         RenderScene();
 
-        Window->SwapBuffers();
+        WindowPtr->SwapBuffers();
     }
 }
 
@@ -55,26 +84,127 @@ void GRenderer::InitWindow()
 
 void GRenderer::MakeWindow()
 {
-    Window = std::unique_ptr<GWindow>(new GWindow("Hello Triangle!"));
+    WindowPtr = std::unique_ptr<GWindow>(new GWindow("Hello Triangle!"));
 }
 
 int GRenderer::PostWindowInit()
 {
-    Window->SetAsContext();
-    Window->SetupViewport();
+    WindowPtr->SetAsContext();
+    WindowPtr->SetupViewport();
 
-    if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
+    CompileShaders();
+
+    float Vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    unsigned int VBO = 0;
+    glGenBuffers(1, &VBO);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+
+    // Bind and send vertex data
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
+    // Set vertex attribute pointer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     return 0;
+}
+
+void GRenderer::CompileShaders()
+{
+    const char* VertexShaderSource = "#version 330 core\n"
+        "layout(location = 0) in vec3 Pos;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(Pos.x, Pos.y, Pos.z, 1.0);\n"
+        "}\0";
+
+
+    const char* FragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragmentColour;\n"
+        "void main()\n"
+        "{\n"
+            "FragmentColour = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+        "}\0";
+
+    VertexShaderPtr = std::unique_ptr<GraphicsShader>(new GraphicsShader(glCreateShader(GL_VERTEX_SHADER)));
+    FragmentShaderPtr = std::unique_ptr<GraphicsShader>(new GraphicsShader(glCreateShader(GL_FRAGMENT_SHADER)));
+
+    ASSERT(
+        CompileShader(VertexShaderSource, *VertexShaderPtr) &&
+        CompileShader(FragmentShaderSource, *FragmentShaderPtr)
+    );
+
+    ShaderProgramPtr = std::unique_ptr<GraphicsShader>(new GraphicsShader(glCreateProgram()));
+
+    std::vector<GraphicsShader> ShaderArray(2);
+    ShaderArray.push_back(*VertexShaderPtr);
+    ShaderArray.push_back(*FragmentShaderPtr);
+    LinkShader(*ShaderProgramPtr, ShaderArray);
+
+    glUseProgram(*ShaderProgramPtr);
+}
+
+bool GRenderer::CompileShader(const char* ShaderSource, GraphicsShader& Shader)
+{
+    glShaderSource(Shader, 1, &ShaderSource, NULL);
+    glCompileShader(Shader);
+
+    int Success = -1;
+    glGetShaderiv(Shader, GL_COMPILE_STATUS, &Success);
+
+    if (!Success)
+    {
+        char InfoLog[512] = { 0 };
+        glGetShaderInfoLog(Shader, 512, NULL, InfoLog);
+        std::cout << "Error: Shader compilation failed - " << InfoLog << std::endl;
+    }
+
+    return Success == 0;
+}
+
+bool GRenderer::LinkShader(GraphicsShader& ShaderProgram, std::vector<GraphicsShader> ShaderArray)
+{
+    for (GraphicsShader Shader : ShaderArray)
+    {
+        glAttachShader(*ShaderProgramPtr, Shader);
+    }
+
+    glLinkProgram(*ShaderProgramPtr);
+
+    int Success = -1;
+    glGetProgramiv(*ShaderProgramPtr, GL_LINK_STATUS, &Success);
+
+    if (!Success)
+    {
+        char InfoLog[512] = { 0 };
+        glGetProgramInfoLog(*ShaderProgramPtr, 512, NULL, InfoLog);
+        std::cout << "Error: Shader linking failed - " << InfoLog << std::endl;
+    }
+
+    return Success == 0;
 }
 
 void GRenderer::RenderScene()
 {
-    Window->ClearToBackground();
+    WindowPtr->ClearToBackground();
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 void GRenderer::Terminate()
