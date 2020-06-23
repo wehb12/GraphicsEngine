11de33031c37cac16ea229cd6d6d832b6e711147
Will <wehb@hotmail.co.uk>
2020-06-23 22:00:51 +0100

    Specular light

--- Source/Graphics/Shaders/Simple/FragmentShader.glsl
+++ Source/Graphics/Shaders/Simple/FragmentShader.glsl
@@ -8,6 +8,7 @@ out vec4 FragmentColour;
 
 uniform vec4 LightColour;
 uniform vec3 LightPosition;
+uniform vec3 CameraPosition;
 
 void main()
 {
@@ -19,5 +20,12 @@ void main()
     float DiffuseStrength = max(dot(Normal, LightDirection), 0.0);
     vec3 Diffuse = vec3(DiffuseStrength * vec3(LightColour));
 
-    FragmentColour = vec4((Ambient + Diffuse), 1.0) * VertexColour;
+    float SpecularStrength = 0.5;
+    vec3 ViewDirection = normalize(CameraPosition - FragmentPosition);
+    vec3 ReflectionDirection = reflect(-LightDirection, Normal);
+    float Shininess = 32;
+    float SpecularPower = pow(max(dot(ViewDirection, ReflectionDirection), 0.0), Shininess);
+    vec3 Specular = vec3(SpecularStrength * SpecularPower * LightColour);
+
+    FragmentColour = vec4((Ambient + Diffuse + Specular), 1.0) * VertexColour;
 }
--- Source/Graphics/Shaders/Textured/FragmentShader.glsl
+++ Source/Graphics/Shaders/Textured/FragmentShader.glsl
@@ -10,6 +10,7 @@ out vec4 FragmentColour;
 uniform sampler2D Texture;
 uniform vec4 LightColour;
 uniform vec3 LightPosition;
+uniform vec3 CameraPosition;
 
 void main()
 {
@@ -20,7 +21,14 @@ void main()
     vec3 LightDirection = normalize(LightPosition - FragmentPosition);
     float DiffuseStrength = max(dot(Normal, LightDirection), 0.0);
     vec3 Diffuse = vec3(DiffuseStrength * LightColour);
+    
+    float SpecularStrength = 0.5;
+    vec3 ViewDirection = normalize(CameraPosition - FragmentPosition);
+    vec3 ReflectionDirection = reflect(-LightDirection, Normal);
+    float Shininess = 32;
+    float SpecularPower = pow(max(dot(ViewDirection, ReflectionDirection), 0.0), Shininess);
+    vec3 Specular = vec3(SpecularStrength * SpecularPower * LightColour);
 
     vec4 TextureColour = texture(Texture, VertexTexCoord);
-    FragmentColour = vec4((Ambient + Diffuse), 1.0) * VertexColour * TextureColour;
+    FragmentColour = vec4((Ambient + Diffuse + Specular), 1.0) * VertexColour * TextureColour;
 }
